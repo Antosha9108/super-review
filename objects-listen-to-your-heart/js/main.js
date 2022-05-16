524ebba65bd37897e4fe7a8312dc992723fc01a9
Antosha9108 <shelkovnikov9108@gmail.com>
2022-05-15 20:25:43 -0400

    added objects listen to your heart

---
+++ objects-listen-to-your-heart/js/main.js
@@ -0,0 +1,20 @@
+//Create a stopwatch object that has four properties and three methods
+
+let stopwatch = {
+    
+}
+
+stopwatch.shape = "round"
+stopwatch.color = "black"
+stopwatch.smell = "new"
+stopwatch.brand = 'accusplit'
+
+stopwatch.start = function(){
+    console.log('Run')
+}
+stopwatch.stop = function(){
+    console.log(' Dont Run')
+}
+stopwatch.morph = function(){
+    console.log('Its morphin time!')
+}
